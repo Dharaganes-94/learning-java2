7af130721a9143a75a891b8f3b3d70d8eadb989b
dharani <dharani.gsd@gmail.com>
2023-09-06 12:11:17 +0530

    first commit

---
+++ Firstprogram.java
@@ -0,0 +1,20 @@
+package Task8;
+
+import java.util.Scanner;
+
+public class Firstprogram{
+	
+   public static void main(String[] args){
+	   	
+	Scanner sc = new Scanner(System.in);
+	int a = sc.nextInt();
+	int b = sc.nextInt();
+	int c = sc.nextInt();
+	int d = sc.nextInt();
+	if((a+b) > (c+d)){
+		System.out.println("The sum of the a and b is gtreater than the sum of c and d");
+	}
+	
+}
+
+}
