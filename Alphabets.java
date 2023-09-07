9da913198d438f90f294782d44ee79e8d1b0afbb
dharani <dharani.gsd@gmail.com>
2023-09-07 21:51:31 +0530

    Added new file Alphabets.java

---
+++ Alphabets.java
@@ -0,0 +1,19 @@
+package Task8;
+
+import java.util.Scanner;
+
+public class Alphabets {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+		
+		char c;
+		
+		for(c = 'A'; c <= 'Z'; ++c)
+		      System.out.print(c + " ");
+
+	}
+
+}
