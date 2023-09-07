1cbf719e4d1edbd63ece3cdb3898283e41894a22
dharani <dharani.gsd@gmail.com>
2023-09-07 23:15:17 +0530

    Added the new file String

---
+++ Stringlength.java
@@ -0,0 +1,15 @@
+package Task8;
+
+import java.util.Scanner;
+
+public class Stringlength {
+
+	public static void main(String[] args) {
+		
+		String s = "Guvi Geek";
+		
+		System.out.println("The length of the given string is"+s.length());
+
+	}
+
+}
