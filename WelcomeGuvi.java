8993c912b7a2dd35150a66ef9dab006babfac37a
dharani <dharani.gsd@gmail.com>
2023-09-11 16:16:33 +0530

    Added the the file Welcome Guvi

---
+++ WelcomeGuvi.java
@@ -0,0 +1,15 @@
+package Task8;
+
+public class WelcomeGuvi {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		for(int i=0;i<10;i++)
+		{
+			System.out.println("Welcome Guvi");
+		}
+
+	}
+
+}
