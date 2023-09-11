c8cb718d48b814becad7b157a56dc0acf0d0b3ca
dharani <dharani.gsd@gmail.com>
2023-09-11 18:49:03 +0530

    Added the two new files

---
+++ CountNumber.java
@@ -0,0 +1,25 @@
+package Task8;
+
+import java.util.Scanner;
+
+public class CountNumber {
+
+	public static void main(String[] args){
+		
+          int count = 0, num = 123456;
+          
+		for (; num != 0; num /= 10, ++count) {
+			
+			    }
+		System.out.println("Number of digits: " + count);
+		
+		
+}
+	
+			
+}
+
+		
+
+
+
---
+++ SeniorCitizen.java
@@ -0,0 +1,24 @@
+package Task8;
+
+import java.util.Scanner;
+
+public class SeniorCitizen {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int age;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the age of the person:");
+	    age = sc.nextInt();
+	    
+	    if(age>=60)
+	    {
+	    	System.out.println("The person is Senior Citizen");
+	    }
+	    
+	    else
+	    	System.out.println("The person is not Senior Citizen");
+	}
+
+}
