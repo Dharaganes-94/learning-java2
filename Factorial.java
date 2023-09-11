d0ee2a27f4a8dc0452fea54cd2491e6af247120b
dharani <dharani.gsd@gmail.com>
2023-09-11 16:08:13 +0530

    Added factorial file

---
+++ Factorial.java
@@ -0,0 +1,20 @@
+package Task8;
+
+public class Factorial {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int factorial = 5;
+		long factResult = 1;
+		for(int i=1;i<=factorial;i++)
+		{
+		factResult = factResult*i;
+		
+		}
+		
+		System.out.println("The factorial value of the number is  "+factResult);
+
+	}
+
+}
